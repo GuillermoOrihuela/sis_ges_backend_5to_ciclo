b7a40f952d865d19afb7711e7ba04a05ff16c6ba
guillermo <nicheaventura1@gmail.com>
2026-09-21 15:11:20 -0500

    verificacione de errores con el fronend2

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,7 +16,13 @@ SECRET_KEY = env('SECRET_KEY', default='django-insecure-key-for-dev')
 
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = [
+    'julio32.pythonanywhere.com',
+    '.pythonanywhere.com',
+    'localhost',
+    '127.0.0.1',
+    '*',
+]
 
 # Aplicaciones instaladas según Contrato Técnico
 INSTALLED_APPS = [
