41506449837c873bce66a7b0ab89cf18f69f9ad2
guillermo <nicheaventura1@gmail.com>
2026-09-21 21:18:31 -0500

    verificacione de errores con el fronend11

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,1 +1,10 @@
-from .production import *
+from .base import *
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/home/julio32/sis_ges_backend_5to_ciclo/db.sqlite3',
+    }
+}
